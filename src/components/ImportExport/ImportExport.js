191da882b5619058a935083f54779a52d22e395c
Svetlin Tanyi <svetlintanyi@gmail.com>
2019-10-29 11:30:37 +0100

    Import JSON File

--- src/components/ImportExport/ImportExport.js
+++ src/components/ImportExport/ImportExport.js
@@ -2,33 +2,58 @@
  * @author Svetlin Tanyi <szvetlin@aitia.ai> on 2019. 10. 29.
  */
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import CloudUpload from '@material-ui/icons/CloudUpload'
 import CloudDownload from '@material-ui/icons/CloudDownload'
 import ListItemText from '@material-ui/core/ListItemText'
+import Files from 'react-files'
 
-class ImportExport extends Component{
-  render(){
-    const { classes } = this.props
+class ImportExport extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      jsonFile: {}
+    }
+
+    this.fileReader = new FileReader()
+    this.fileReader.onload = event => {
+      this.setState({jsonFile: JSON.parse(event.target.result)}, () => {
+        console.log(this.state.jsonFile)
+      })
+    }
+  }
+
+
+  render() {
+    const {classes} = this.props
     return (
       <div className={classes.importExport}>
         <List className={classes.list}>
           <ListItem
             button
-            onClick={() => console.log('click')}
             className={classes.itemLink}
           >
             <ListItemIcon className={classes.itemIcon}>
-              <CloudUpload />
+              <CloudUpload/>
             </ListItemIcon>
-            <ListItemText
-              primary="Import"
+            <Files
               className={classes.itemText}
-              disableTypography
-            />
+              onChange={file => {
+                this.fileReader.readAsText(file[0])
+              }}
+              onError={err => console.log(err)}
+              accepts={['.json']}
+              multiple={false}
+              maxFileSize={10000000}
+              minFileSize={0}
+              clickable>
+              <div style={{paddingLeft: '16px', paddingRight: '116px'}}>
+                Import
+              </div>
+            </Files>
           </ListItem>
           <ListItem
             button
@@ -36,7 +61,7 @@ class ImportExport extends Component{
             className={classes.itemLink}
           >
             <ListItemIcon className={classes.itemIcon}>
-              <CloudDownload />
+              <CloudDownload/>
             </ListItemIcon>
             <ListItemText
               primary="Export"
